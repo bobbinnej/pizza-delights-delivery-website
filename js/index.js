f19317d0397c960bf482503db7a5fb605de7b960
jackie <bobbinnej@gmail.com>
2022-03-19 22:26:42 +0300

    End of contact section

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
-$(document).ready(function(){
+ function submitBtn(){
+     let name1=document.getElementById("name").value
+     let email1=document.getElementById("email").value
+     let textarea1=document.getElementById("exampleFormControlTextarea1").value
 
+     if( name1 !=="" && email1 !=="" && textarea1 !==""){
 
-});
+        alert("We have recieved your feedback")
+
+     }else{
+         alert("All fields must be filled")
+     }
+ }
