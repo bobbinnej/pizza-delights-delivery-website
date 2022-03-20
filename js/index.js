df1a91a26f18a54df03e5ef409bbe13c7eca7b00
jackie <bobbinnej@gmail.com>
2022-03-20 21:35:11 +0300

    Add readme file

--- js/index.js
+++ js/index.js
@@ -29,25 +29,122 @@
 
  });
 
- function okBtn(){
-     let delivery=document.getElementById("delivery-address").value;
+//  function okBtn(){
+//      let delivery=document.getElementById("delivery-address").value;
 
-     if(delivery !="Y"){
-         alert(`Your order will be delivered to the address ${delivery} at a Kes 60 Delivery Fee` )
-     }else{
-         alert("Delivery address must be filled");
-     }
- }
+//      if(delivery !="Y"){
+//          alert(`Your order will be delivered to the address ${delivery} at a Kes 60 Delivery Fee` )
+//      }else{
+//          alert("Delivery address must be filled");
+//      }
+//  }
 
- /*Gathering user input*/
+ /*Gathering user input using jquery*/
  $(document).ready(function(){
      $("form#pizza-form").submit(function(e){
          e.preventDefault()
          let pizzaNumber=$("input#number-of-pizzas").val()
-         console.log(pizzaNumber)
+         let pizzaSize=$("#pizza-size").find(":selected").text();
+         let pizzaCrust=$("#pizza-crust").find(":selected").text();
+         let pizzaTopping=$("#pizza-topping").find(":selected").text();
+         let pizzaDelivery=$("#pizza-delivery").find(":selected").text();
+         let pizzaAddress=$("#delivery-address").find(":selected").text();
+         
+         const pizza1= new Pizza(pizzaNumber,pizzaSize,pizzaCrust,pizzaTopping,pizzaDelivery,pizzaAddress)
+         console.log(pizza1)
+         pizza1.calculatePrice()
+         
 
-     });
+     }) 
+
+     /*Pizza form constructor */
+     function Pizza(pizzaNumber,pizzaSize,pizzaCrust,pizzaTopping,pizzaDelivery,pizzaAddress){
+         this.pizzaNumber=pizzaNumber;
+         this.pizzaSize=pizzaSize;
+         this.pizzaCrust=pizzaCrust;
+         this.pizzaTopping=pizzaTopping;
+         this.pizzaDelivery=pizzaDelivery;
+         this.pizzaAddress=pizzaAddress;
+
+     }
+     /**Pizza prototype */
+     Pizza.prototype.calculatePrice = function(){
+         console.log("total calculated")
+         let numberPrice;
+         let deliveryPrice=60;
+         let sizePrice;  
+
+          if(this.pizzaSize==="Mega (1200)"){
+              sizePrice=1200
+
+          }else if(this.pizzaSize==="Large (1000)"){
+             sizePrice=1000
+          }
+          else if(this.pizzaSize==="Medium (800)"){
+             sizePrice=800
+         }else{
+             sizePrice=550
+         }
+
+         let crustPrice;
+         if(this.pizzaCrust==="Gluten-free (200)"){
+            crustPrice=200
+         }else if(this.pizzaCrust==="Stuffed (150)"){
+             crustPrice=150
+         }
+         else if(this.pizzacrust==="Thick (100)"){
+            crustPrice=100
+            
+         }else{
+             crustPrice=50
+         }
+
+         let toppingPrice;
+         if(this.pizzaTopping==="Pepperoni (20)"){
+            toppingPrice=20
+         }else if(this.pizzaTopping==="Mushroom (20)"){
+             toppingPrice=20
+         }
+         else if(this.pizzaTopping==="Onions (10)"){
+            toppingPrice=10
+            
+         }
+         else if(this.pizzaTopping==="Sausage (60)"){
+            toppingPrice=60
+            
+         }
+         else if(this.pizzaTopping==="Extra-cheese (80)"){
+            toppingPrice=80
+            
+         }
+         else if(this.pizzaTopping==="Black-olives (30)"){
+            toppingPrice=30
+            
+         }
+         else if(this.pizzaTopping==="Green-pepper (30)"){
+            toppingPrice=30
+            
+         }
+         else if(this.pizzaTopping==="Pineapple (40)"){
+            toppingPrice=40
+            
+         }
+         
+         else{
+             toppingPrice=30
+         }
+
+         
+
+         /*Total */
+        let totalPizzaPrice=sizePrice+crustPrice+toppingPrice
+        console.log(totalPizzaPrice)
+        
+
+        
+     }
 
  });
+
 
  
